d5e89ed4137e1db67251c6110f17fd87fe8a6c02
chrismuwang <christina.muyun.wang@gmail.com>
2022-08-17 16:26:20 +0000

    Testing webhook

--- src/main/java/com/example/demo/LoadDatabase.java
+++ src/main/java/com/example/demo/LoadDatabase.java
@@ -19,6 +19,7 @@ class LoadDatabase {
             log.info("Preloading " + repository.save(new MusicItem("Ghost Town", "The Specials", "Ska")));
             log.info("Preloading " + repository.save(new MusicItem("Let There Be Rock", "ACDC", "Hard Rock")));
             log.info("Preloading " + repository.save(new MusicItem("Stairway to Heaven", "Led Zeppelin", "Heavy Metal")));
+            log.info("Preloading " + repository.save(new MusicItem("Free Loop", "Daniel Powter", "Pop")));
         };
     }
 }
